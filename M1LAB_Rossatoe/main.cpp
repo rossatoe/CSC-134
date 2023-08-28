deb852fa63d5b5b42dfdd9dccf0f1d16776fd1b1
rossatoe <rossatoe4232@student.faytechcc.edu>
2023-08-28 11:25:52 -0400

    Started m1lab

---
+++ M1LAB_Rossatoe/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    CSC 134
+    M1LAB - Apple Orchard
+    rossatoe
+    08/28
+    Simple interactive store
+    */
+    cout << "Hello world!" << endl;
+    return 0;
+
+}
